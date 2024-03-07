4ac0ecbb66da3cca82d570b795a62ddc4066421e
Ana Arango <anitag.arango@icloud.com>
2024-03-07 04:42:18 -0800

    better padding for buttons

--- src/app/components/FeedbackForm.tsx
+++ src/app/components/FeedbackForm.tsx
@@ -40,15 +40,15 @@ export default function FeedbackForm({ showFeedback = false, closeFeedback = () 
     <>
       {showFeedback &&
         <div className="w-screen h-screen absolute z-40 bg-gray-800/50 flex justify-center items-center">
-          <form action="/" onSubmit={(e) => submitForm(e)} className="bg-white p-5 rounded-lg w-3/6 max-w-[600px]">
+          <form action="/" onSubmit={(e) => submitForm(e)} className="bg-white p-5 rounded-lg w-5/6 max-w-[600px]">
             <div>
               <h1 className='text-xl font-bold'>Got A Suggestion?</h1>
               <p className="py-4">Let me know if there are any well-known characters you&apos;d like to chat with (or if you encountered any bugs 🙈).</p>
-              <input className="bg-[#D9F3EB] w-full p-2 rounded-md text-sm outline-none" type="email" placeholder="Email:" required ref={emailRef} />
-              <textarea placeholder="Tell me what you think..." rows={2} className="outline-none p-2 rounded-md my-2 bg-[#D9F3EB] w-full text-sm" required ref={messageRef}></textarea>
+              <input className="bg-[#D9F3EB] w-full p-2 rounded-md outline-none" type="email" placeholder="Email:" required ref={emailRef} />
+              <textarea placeholder="Tell me what you think..." rows={2} className="outline-none p-2 rounded-md my-2 bg-[#D9F3EB] w-full" required ref={messageRef}></textarea>
               <div className="flex justify-end gap-3 pt-5">
-                <button type="submit" disabled={idleEmail} className='bg-[#50A98D] text-white py-1 px-2 rounded-md cursor-pointer'>{idleEmail ? <Image alt="loading gif" src="/spinner.svg" width={70} height={70} className="w-[50px] h-5" /> : "Submit"}</button>
-                <button onClick={() => closeFeedback(false)} className='text-[#50A98D] bg-gray-200 py-1 px-2 rounded-md cursor-pointer'>Cancel</button>
+                <button type="submit" disabled={idleEmail} className='bg-[#50A98D] text-white py-2 px-3 rounded-md cursor-pointer'>{idleEmail ? <Image alt="loading gif" src="/spinner.svg" width={70} height={70} className="w-[50px] h-5" /> : "Submit"}</button>
+                <button onClick={() => closeFeedback(false)} className='text-[#50A98D] bg-gray-200 py-2 px-3  rounded-md cursor-pointer'>Cancel</button>
               </div>
             </div>
           </form>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 export default function Header({setViewFeedback}:{setViewFeedback:Function}){
   const r = useRouter()
   return(
-    <div className='bg-white px-3 py-4 flex w-full justify-between items-center'>
+    <div className='bg-white p-3 flex w-full justify-between items-center'>
       <Image alt="fictichat logo" width={100} height={100} onClick={()=>r.push("/")} className='w-32 cursor-pointer' src="/fictichat.svg"/>
       <div className='flex gap-3'>
         <button className='cursor-pointer duration-200 hover:text-[#50A98D]' onClick={()=>{setViewFeedback(true)}}>Feedback</button>
-        <button className='bg-[#50A98D] text-white py-1 px-2 rounded-md cursor-pointer duration-200 hover:bg-[#97D8C4]'>Sign In</button>
+        <button className='bg-[#50A98D] text-white py-2 px-3 rounded-md cursor-pointer duration-200 hover:bg-[#97D8C4]'>Sign In</button>
       </div>
     </div>
   )
