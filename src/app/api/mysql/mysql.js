0c8172ff164a3fceb8a7ee8fa26eb388063b56b4
Ana Arango <anitag.arango@icloud.com>
2024-03-12 21:48:36 -0700

    added new hosting for mysql

--- src/app/api/mysql/mysql.js
+++ src/app/api/mysql/mysql.js
@@ -1,8 +1,19 @@
 import mysql from "mysql2/promise"
 const connection = mysql.createConnection({
   host: process.env.MYSQLHOST,
-  user:  'root',
+  user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+connection.query("SELECT 1+1").on("result", function (row) {
+  console.log(row);
+});
+
 export default connection;
