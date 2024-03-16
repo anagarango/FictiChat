43b3d6d0e609a9b7078aad3761673ccfd766c7d1
Ana Arango <anitag.arango@icloud.com>
2024-03-16 01:32:43 -0700

    added mobile menu

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -21,6 +21,7 @@ export default function Header({currentUserId, setCurrentUserId=()=>{}, setCurre
   const [switchTheme, setSwitchTheme] = useState<string | undefined>("")
   const [currentPage, setCurrentPage] = useState<string>()
   const [viewModal, setViewModal] = useState<string>("")
+  const [showMenu, setShowMenu] = useState<string>("opacity-0")
 
   useEffect(() => {
     setSwitchTheme(resolvedTheme)
@@ -103,17 +104,28 @@ export default function Header({currentUserId, setCurrentUserId=()=>{}, setCurre
     <>
     <div id="header" className='bg-white p-3 flex w-full justify-between items-center'>
       <div className='flex gap-4 items-center'>
-        <Image alt="fictichat logo" width={100} height={100} onClick={()=>handleCurrentPage("/")} className='w-32 cursor-pointer' src="/fictichat.svg"/>
-        <p className={`cursor-pointer font-bold ${currentPage == "/" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/")}>Home</p>
-        <p className={`cursor-pointer font-bold ${currentPage == "/characters" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/characters")}>Characters</p>
+        <Image alt="fictichat logo" width={100} height={100} onClick={()=>handleCurrentPage("/")} className='w-28 cursor-pointer' src="/fictichat.svg"/>
+        <p id="header-desktop" className={`cursor-pointer font-bold ${currentPage == "/" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/")}>Home</p>
+        <p id="header-desktop" className={`cursor-pointer font-bold ${currentPage == "/characters" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/characters")}>Characters</p>
       </div>
       
-      <div className='flex gap-4 items-center'>
+      <div id="header-desktop" className='flex gap-4 items-center'>
         <button className='cursor-pointer duration-200 hover:text-[#50A98D]' onClick={()=>{setViewModal("feedback")}}>Feedback</button>
         <Image alt={switchTheme || "icon"} onClick={()=>{toggleTheme()}} src={switchTheme == "light" ? "/dark.png" : "/light.png"} width={10} height={10} className="flex w-auto h-[25px] cursor-pointer"/>
         <button className='bg-[#287B62] text-white py-2 px-3 rounded-md cursor-pointer duration-200 hover:bg-[#50A98D]' onClick={()=>{currentUserId ? logOut() : setViewModal("login")}}>{currentUserId ? "Sign Out": "Sign In"}</button>
       </div>
+
+      <Image id="header-mobile" alt="menu icon" onClick={()=>{setShowMenu(showMenu == "opacity-0" ? "opacity-100" : "opacity-0" )}} src="/menu.png" width={10} height={10} className="flex w-auto h-[25px] cursor-pointer"/>
+
+      <div id="header-mobile" className={`absolute right-0 bg-slate-900 duration-200 top-14 ${showMenu}`}>
+        <div className={`cursor-pointer font-bold p-2  ${currentPage == "/" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/")}>Home</div>
+        <div className={`cursor-pointer font-bold p-2 ${currentPage == "/characters" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/characters")}>Characters</div>
+        <div className="p-2 border-t-2 border-gray-700 cursor-pointer duration-200 hover:text-[#50A98D]" onClick={()=>{setViewModal("feedback")}}>Feedback</div>
+        <div className="p-2 border-t-2 border-gray-700 bg-[#287B62] text-white py-2 px-3 cursor-pointer duration-200 hover:bg-[#50A98D]" onClick={()=>{currentUserId ? logOut() : setViewModal("login")}}>{currentUserId ? "Sign Out": "Sign In"}</div>
+      </div>
+      
     </div>
+    
     {viewModal == "feedback" && <FeedbackForm showModal={viewModal} closeFeedback={setViewModal}/>}
     {viewModal == "login" && <LogIn showModal={viewModal} closeFeedback={setViewModal} currentUserId={(e:SessionStorage)=>GrabAllUsersChats(e)}/>}
     </>
