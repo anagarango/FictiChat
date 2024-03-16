69f635511289e4f7ee7b6cb19ddf08c32570a626
Ana Arango <anitag.arango@icloud.com>
2024-03-16 01:43:12 -0700

    altered light dark mode for mobile menu

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -105,8 +105,8 @@ export default function Header({currentUserId, setCurrentUserId=()=>{}, setCurre
     <div id="header" className='bg-white p-3 flex w-full justify-between items-center'>
       <div className='flex gap-4 items-center'>
         <Image alt="fictichat logo" width={100} height={100} onClick={()=>handleCurrentPage("/")} className='w-28 cursor-pointer' src="/fictichat.svg"/>
-        <p id="header-desktop" className={`cursor-pointer font-bold ${currentPage == "/" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/")}>Home</p>
-        <p id="header-desktop" className={`cursor-pointer font-bold ${currentPage == "/characters" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/characters")}>Characters</p>
+        <p id="header-desktop" className={`cursor-pointer font-bold ${currentPage == "/" ? "text-[#50A98D]" : "text-[#989898]"}`} onClick={()=>handleCurrentPage("/")}>Home</p>
+        <p id="header-desktop" className={`cursor-pointer font-bold ${currentPage == "/characters" ? "text-[#50A98D]" : "text-[#989898]"}`} onClick={()=>handleCurrentPage("/characters")}>Characters</p>
       </div>
       
       <div id="header-desktop" className='flex gap-4 items-center'>
@@ -115,13 +115,17 @@ export default function Header({currentUserId, setCurrentUserId=()=>{}, setCurre
         <button className='bg-[#287B62] text-white py-2 px-3 rounded-md cursor-pointer duration-200 hover:bg-[#50A98D]' onClick={()=>{currentUserId ? logOut() : setViewModal("login")}}>{currentUserId ? "Sign Out": "Sign In"}</button>
       </div>
 
-      <Image id="header-mobile" alt="menu icon" onClick={()=>{setShowMenu(showMenu == "opacity-0" ? "opacity-100" : "opacity-0" )}} src="/menu.png" width={10} height={10} className="flex w-auto h-[25px] cursor-pointer"/>
+      <div id="header-mobile" className='flex gap-4 items-center'>
+        <Image alt={switchTheme || "icon"} onClick={()=>{toggleTheme()}} src={switchTheme == "light" ? "/dark.png" : "/light.png"} width={10} height={10} className="flex w-auto h-[25px] cursor-pointer"/>
+        <Image alt="menu icon" onClick={()=>{setShowMenu(showMenu == "opacity-0" ? "opacity-100" : "opacity-0" )}} src="/menu.png" width={10} height={10} className="flex w-auto h-[25px] cursor-pointer"/>
+      </div>
+      
 
-      <div id="header-mobile" className={`absolute right-0 bg-slate-900 duration-200 top-14 ${showMenu}`}>
-        <div className={`cursor-pointer font-bold p-2  ${currentPage == "/" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/")}>Home</div>
-        <div className={`cursor-pointer font-bold p-2 ${currentPage == "/characters" ? "text-[#50A98D]" : "text-[#CDCDCD]"}`} onClick={()=>handleCurrentPage("/characters")}>Characters</div>
-        <div className="p-2 border-t-2 border-gray-700 cursor-pointer duration-200 hover:text-[#50A98D]" onClick={()=>{setViewModal("feedback")}}>Feedback</div>
-        <div className="p-2 border-t-2 border-gray-700 bg-[#287B62] text-white py-2 px-3 cursor-pointer duration-200 hover:bg-[#50A98D]" onClick={()=>{currentUserId ? logOut() : setViewModal("login")}}>{currentUserId ? "Sign Out": "Sign In"}</div>
+      <div id="header-mobile" className={`flex-col absolute right-0 ${switchTheme == "light" ? "bg-slate-200 border-gray-300" : "bg-slate-900 border-gray-600"} duration-200 top-14 z-10 border-2  ${showMenu}`}>
+        <div className={`cursor-pointer font-bold p-2  ${currentPage == "/" ? "text-[#50A98D]" : "text-[#989898]"}`} onClick={()=>handleCurrentPage("/")}>Home</div>
+        <div className={`cursor-pointer font-bold p-2 ${currentPage == "/characters" ? "text-[#50A98D]" : "text-[#989898]"}`} onClick={()=>handleCurrentPage("/characters")}>Characters</div>
+        <div className={`p-2 border-t-2 ${switchTheme == "light" ? "border-gray-300" : "border-gray-600"} cursor-pointer duration-200 hover:text-[#50A98D]`} onClick={()=>{setViewModal("feedback")}}>Feedback</div>
+        <div className="p-2 bg-[#287B62] text-white py-2 px-3 cursor-pointer duration-200 hover:bg-[#50A98D]" onClick={()=>{currentUserId ? logOut() : setViewModal("login")}}>{currentUserId ? "Sign Out": "Sign In"}</div>
       </div>
       
     </div>
