e895537b814cc42acfe6ad2686ce680952773e80
anagarango <anitag.arango@icloud.com>
2024-05-14 17:45:46 -0700

    new update to api call

--- src/app/api/char/route.ts
+++ src/app/api/char/route.ts
@@ -1,25 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server'
 const { OpenAI } = require("openai")
 
-
-
-interface ChatObject {
-  user:string,
-  message:string,
-  role:string
-}
-
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
     const character = searchParams.get('character');
+    const message = searchParams.get('message');
     const apiKey = searchParams.get('apiKey');
 
-    const requestData = await request.json();
-    const formattedConversation = requestData.context.map(({ message, role }:ChatObject) => ({
-      role,
-      content: message
-    }));
+    // const requestData = await request.json();
+    // const formattedConversation = requestData.context.map(({ message, role }:ChatObject) => ({
+    //   role,
+    //   content: message
+    // }));
 
     const openai = new OpenAI({
       apiKey
@@ -28,7 +21,7 @@ export async function POST(request: NextRequest) {
     const completion = await openai.chat.completions.create({
       messages: [
         { role: 'system', content: `I want you to respond and answer like ${character} using the tone, manner and vocabulary ${character} would use. You must know all of the knowledge of ${character}. Answer logically to whats been asked to you.` },
-        ...formattedConversation,
+        { role: 'user', content: message}
       ],
       model: 'gpt-3.5-turbo',
     });
