00e50a9642af536791e8df6fc9a59109ac0b6d28
anagarango <anitag.arango@icloud.com>
2024-05-14 17:16:57 -0700

    added api route for other people

---
+++ src/app/api/char/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from 'next/server'
+const { OpenAI } = require("openai")
+
+
+
+interface ChatObject {
+  user:string,
+  message:string,
+  role:string
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const character = searchParams.get('character');
+    const apiKey = searchParams.get('apiKey');
+
+    const requestData = await request.json();
+    const formattedConversation = requestData.context.map(({ message, role }:ChatObject) => ({
+      role,
+      content: message
+    }));
+
+    const openai = new OpenAI({
+      apiKey
+    })
+
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: 'system', content: `I want you to respond and answer like ${character} using the tone, manner and vocabulary ${character} would use. You must know all of the knowledge of ${character}. Answer logically to whats been asked to you.` },
+        ...formattedConversation,
+      ],
+      model: 'gpt-3.5-turbo',
+    });
+
+    return NextResponse.json({ message: completion.choices[0].message.content}, { status: 200 })
+  } catch (error) {
+    console.error('Error processing request:', error);
+    return NextResponse.error();
+  }
+}
